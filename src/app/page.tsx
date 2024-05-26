a3aa913f737d4edd1d276072dfa147d065190d5e
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2024-05-26 12:40:13 +0600

    Lesson 19 done.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import Link from "next/link" // needs to be imported here in order for Link tag to work (for Navigation)
+
 const Home = () => {
   return (
-    <h1>Welcome Home</h1>
+    <>
+      <h1>Welcome Home</h1>
+      <Link href="/blog">To Blog Page</Link>
+      <br />
+      <Link href="/products">To Products Page</Link>
+    </>
   )
 }
 
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,19 @@
+import Link from "next/link"
+
 const ProductList = () => {
+  const productId = 100
+
   return (
     <>
       <h1>ProductList</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <h2><Link href="/products/1">Product 1</Link></h2>
+      <h2><Link href="/products/2">Product 2</Link></h2>
+      
+      <h2><Link href="/products/3" replace>Product 3</Link></h2>
+
+      <h2><Link href={`/products/${productId}`}>Product {productId}</Link></h2>
+
+      <Link href="/">Back To Home</Link>
     </>
   )
 }
