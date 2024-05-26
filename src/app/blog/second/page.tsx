bb745ef7f5fe42ebda7fab40d4017a3de1d4e896
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2024-05-26 12:04:12 +0600

    Done upto lesson 19.

--- src/app/blog/second/page.tsx
+++ src/app/blog/second/page.tsx
@@ -1,3 +1,10 @@
+export const metadata : Metadata = {
+  // a title metadata can have these 3 segments:
+  title: {
+    absolute: 'Override layout title metadata', // Applied if not blank and if child routes don't have metadata titles i.e setting this on child overrides layout/parent metadata rules
+  },
+}
+
 const Second = () => {
   return (
     <div>Second</div>
