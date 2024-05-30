8c2a538416fbe708de198ebae62bca3ecb2fc863
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2024-05-30 13:09:21 +0600

    Lesson 39 done.

--- route_handlers/src/app/api/comments/[id]/route.ts
+++ route_handlers/src/app/api/comments/[id]/route.ts
@@ -1,5 +1,31 @@
 import { comments } from "../data";
 
+// IMP NOTE: import and use "NextRequest" from which we can extract query params from the request object. Hence, the rule of thumb is: 
+// 1. when you don't need to use query params, but need context(e.g to extract route params from it.), you use type "Request" i.e -> 
+//      export async function GET(request: Request, { params }: { params: {id: string} }) {
+//          ...
+//            const { type } = params
+//          ...
+//       }
+// 2. when you need to use query params, but don't need context(i.e no route params present but may have query params present), you use type "NextRequest" i.e -> 
+//      export async function GET(request: NextRequest) {
+//          ...
+//            const searchParams = request.nextUrl.searchParams
+//            const limit = searchParams.get("limit")
+//            const offset = searchParams.get("offset")
+//          ...
+//       }
+// 3. when you need both, use a combination of NextRequest and context params(i.e has both route params and query params present), you use type "NextRequest" with { params } i.e -> 
+//      export async function GET(request: NextRequest, { params }: { params: {id: string} }) { 
+//          ...
+//            const { type } = params
+//          ...
+//            const searchParams = request.nextUrl.searchParams
+//            const limit = searchParams.get("limit")
+//            const offset = searchParams.get("offset")
+//          ...
+//       }
+
 // NOTE: 2nd param is supposed to be context, but we are destructuring it here to get route params from it
 export async function GET(request: Request, { params }: { params: {id: string} }) {
 
@@ -24,7 +50,6 @@ export async function PATCH(request: Request, { params }: { params: {id: string}
 
 // NOTE: 2nd param is supposed to be context, but we are destructuring it here to get route params from it
 export async function DELETE(request: Request, { params }: { params: {id: string} }) {
-
   const index = comments.findIndex(comment => comment.id === parseInt(params.id))
   const deletedComment = comments[index]
 
--- route_handlers/src/app/api/comments/route.ts
+++ route_handlers/src/app/api/comments/route.ts
@@ -1,7 +1,58 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
-export async function GET() {
-  return Response.json({ data: comments })
+// IMP NOTE: import and use "NextRequest" from which we can extract query params from the request object. Hence, the rule of thumb is: 
+// 1. when you don't need to use query params, but need context(e.g to extract route params from it.), you use type "Request" i.e -> 
+//      export async function GET(request: Request, { params }: { params: {id: string} }) {
+//          ...
+//            const { type } = params
+//          ...
+//       }
+// 2. when you need to use query params, but don't need context(i.e no route params present but may have query params present), you use type "NextRequest" i.e -> 
+//      export async function GET(request: NextRequest) {
+//          ...
+//            const searchParams = request.nextUrl.searchParams
+//            const limit = searchParams.get("limit")
+//            const offset = searchParams.get("offset")
+//          ...
+//       }
+// 3. when you need both, use a combination of NextRequest and context params(i.e has both route params and query params present), you use type "NextRequest" with { params } i.e -> 
+//      export async function GET(request: NextRequest, { params }: { params: {id: string} }) { 
+//          ...
+//            const { type } = params
+//          ...
+//            const searchParams = request.nextUrl.searchParams
+//            const limit = searchParams.get("limit")
+//            const offset = searchParams.get("offset")
+//          ...
+//       }
+export async function GET(request: NextRequest) {
+  // NOTE: this is how you get query params from URL
+  const searchParams = request.nextUrl.searchParams
+  const search = searchParams.get("search")
+
+  // NOTE: Good ol' procedural way
+  // let filteredComments = []
+  // if(search){
+  //   filteredComments = comments.filter((comment) => {
+  //     if(comment.text.indexOf(search) !== -1)
+  //       return comment
+  //   })
+  // }
+  // else{
+  //   filteredComments = comments
+  // }
+
+  // NOTE: Shortened Way. Replaces block above.
+  let filteredComments = search ? 
+                          comments.filter((comment) => {
+                            if(comment.text.includes(search))
+                              return comment
+                          })
+                          : 
+                          comments
+
+  return Response.json({ data: filteredComments })
 }
 // NOTE: To get post body and 
 export async function POST(request: Request) {
