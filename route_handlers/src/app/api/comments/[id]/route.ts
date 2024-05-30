2fabca863678adcdbd36512df405f4f94212bf8c
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2024-05-30 12:00:42 +0600

    Lesson 39 done.

--- route_handlers/src/app/api/comments/[id]/route.ts
+++ route_handlers/src/app/api/comments/[id]/route.ts
@@ -1,9 +1,42 @@
 import { comments } from "../data";
 
-// 2nd param is supposed to be context, but we are destructuring it here to get params from it
+// NOTE: 2nd param is supposed to be context, but we are destructuring it here to get route params from it
 export async function GET(request: Request, { params }: { params: {id: string} }) {
 
   const findComment = comments.find(comment => comment.id === parseInt(params.id))
 
   return Response.json(findComment)
+}
+
+// NOTE: 2nd param is supposed to be context, but we are destructuring it here to get route params from it
+export async function PATCH(request: Request, { params }: { params: {id: string} }) {
+
+  const body = await request.json()
+  const { text } = body
+
+  const index = comments.findIndex(comment => comment.id === parseInt(params.id))
+  comments[index].text = text
+
+  return Response.json(comments[index], {
+    status: 200 // In case you wish to send a status code other than 200(default)
+  })
+}
+
+// NOTE: 2nd param is supposed to be context, but we are destructuring it here to get route params from it
+export async function DELETE(request: Request, { params }: { params: {id: string} }) {
+
+  const index = comments.findIndex(comment => comment.id === parseInt(params.id))
+  const deletedComment = comments[index]
+
+  if(!deletedComment){
+    return Response.json({ message: "Comment Not Found" }, {
+      status: 404 // In case you wish to send a status code other than 200(default)
+    })
+  }
+
+  comments.splice(index, 1)
+
+  return Response.json(deletedComment, {
+    status: 200 // In case you wish to send a status code other than 200(default)
+  })
 }
--- route_handlers/src/app/api/comments/route.ts
+++ route_handlers/src/app/api/comments/route.ts
@@ -3,14 +3,14 @@ import { comments } from "./data";
 export async function GET() {
   return Response.json({ data: comments })
 }
-// TO get post body and 
+// NOTE: To get post body and 
 export async function POST(request: Request) {
-  // need to await in order to store request data in a variable(since it is promise based)
+  // NOTE: need to await in order to store request data in a variable(since it is promise based)
   const comment = await request.json()
   const newComment = { id: comments.length + 1, text: comment.text }
   comments.push(newComment)
 
-  // "return Response.json({ data: comments })" always sends a status code of 200 so you need to send a new Response object to have a different status code
+  // NOTE: "return Response.json({ data: comments })" always sends a status code of 200 so you need to send a new Response object to have a different status code
   // first params has to be JSON.stringified for new Response to be properly returned. I think that is how the signature of the Response class is.
   return new Response(JSON.stringify(newComment), {
     headers: {
