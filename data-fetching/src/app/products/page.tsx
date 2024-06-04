2c62aee890804dcdca01adb82886f04ef7109cfb
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2024-06-04 18:01:04 +0600

    In the middle of lesson 68. Need to finish the IMP NOTE in page.tsx in products.

--- data-fetching/src/app/products/page.tsx
+++ data-fetching/src/app/products/page.tsx
@@ -9,8 +9,17 @@ const ProductsPage = async () => {
 
   // NOTE: By default, fetch in Next JS caches server data by default so anytime you refresh the page, Next JS will not do another server call so previous data will be shown, so if you change db.json data
   // and restart json-server and refresh the page, you will the same data and not the new data. The cache data is stored in ".next/cache" directory so deleting ".next" folder will force updated data to be fetched.
-  const response = await fetch("http://localhost:3001/products")
-  const products = await response.json()
+
+  // NOTE: Make a request and cache the response data
+  // const response = await fetch("http://localhost:3001/products")
+
+  // IMP NOTE: As per
+
+  // NOTE: For opt-outing out of cacheing
+  const response = await fetch("http://localhost:3001/products", {
+    cache: "no-cache" // This is the most straight-forward way to opt-out of cacheing i.e adding this option
+  })
+  const products = await response.json() // Unwrap response data
 
   console.log(products);
 
@@ -20,13 +29,11 @@ const ProductsPage = async () => {
       <ul className="space-y-4 p-4">
         {products.map((product: Product) => {
           return(
-            <>
-              <li key={product.id} className="p-4 bg-white shadow-md rounded-lg text-gray-700">
-                <h2 className="text-xl font-semibold">{product.title}</h2>
-                <p>{product.description}</p>
-                <p className="text-lg font-medium">${product.price}</p>
-              </li>
-            </>
+            <li key={product.id} className="p-4 bg-white shadow-md rounded-lg text-gray-700">
+              <h2 className="text-xl font-semibold">{product.title}</h2>
+              <p>{product.description}</p>
+              <p className="text-lg font-medium">${product.price}</p>
+            </li>
           )
         })}
       </ul>
--- data-fetching/src/app/users/page.tsx
+++ data-fetching/src/app/users/page.tsx
@@ -8,8 +8,8 @@ type User = {
 
 const UsersPage = async () => {
   await new Promise((resolve) => setTimeout(resolve, 2000)) // delay resolving this component for 2 seconds so we can see loading.tsx
-  const response = await fetch("https://jsonplaceholder.typicode.com/users") // For throwing error and showing error.tsx
-  // const response = await fetch("https://jsonplaceholder.typicode.com/users") 
+  // const response = await fetch("https://jsonplaceholder.typicode.com/users123") // Faulty URL. For throwing error and showing error.tsx
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
   const users = await response.json()
   console.log(users)
 
@@ -18,7 +18,6 @@ const UsersPage = async () => {
       <div className="grid grid-cols-2 gap-2 p-4">
         {users.map((user: User) => {
           return(
-          <>
             <div key={user.id} className="flex justify-between align-center p-4 bg-white shadow rounded-lg text-gray-800">
               <div className="flex flex-col space-y-1">
                 <h2 className="text-lg font-semibold">{user.name}</h2>
@@ -29,7 +28,7 @@ const UsersPage = async () => {
                 <div className="text-md">{user.phone}</div>
               </div>
             </div>
-          </>)
+          )
         })}
       </div>
     </>
