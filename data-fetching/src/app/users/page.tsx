73891d9c465e98a17c865dc3356374adc3b810bb
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2024-06-04 16:14:45 +0600

    Lesson 63 done.

---
+++ data-fetching/src/app/users/page.tsx
@@ -0,0 +1,38 @@
+type User = {
+  id: string,
+  name: string,
+  username: string,
+  email: string,
+  phone: string,
+}
+
+const UsersPage = async () => {
+
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const users = await response.json()
+  console.log(users)
+
+  return (
+    <>
+      <div className="grid grid-cols-2 gap-2 p-4">
+        {users.map((user: User) => {
+          return(
+          <>
+            <div key={user.id} className="flex justify-between align-center p-4 bg-white shadow rounded-lg text-gray-800">
+              <div className="flex flex-col space-y-1">
+                <h2 className="text-lg font-semibold">{user.name}</h2>
+                <h2 className="text-sm">{user.username}</h2>
+              </div>
+              <div className="flex flex-col space-y-1 items-end">
+                <div className="text-md">{user.email}</div>
+                <div className="text-md">{user.phone}</div>
+              </div>
+            </div>
+          </>)
+        })}
+      </div>
+    </>
+  )
+}
+
+export default UsersPage
--- rendering/src/app/product-detail/page.tsx
+++ rendering/src/app/product-detail/page.tsx
@@ -7,7 +7,9 @@ const ProductDetail = () => {
     <>
       <div>Product Detail Page</div>
       {/* NOTE: Streaming. Wrapping delayed components in a suspense to enable streaming for them. This means the page will be rendered immediately without waiting for the Product and Reviews component
-        to finish their async promises to resolve(i.e API calls or otherwise) and once their promises are resolved, the component will be streamed to the browser and placed into the correct spots on the page */}
+        to finish their async promises to resolve(i.e API calls or otherwise) and once their promises are resolved, the component will be streamed to the browser and placed into the correct spots on the page.
+        Remember that are we wrapping this component with Suspense instead of using a loading.tsx because this is a streamed component. For streamed and client-side components, we will need Suspense and 
+        ErrorBoundary tags instead of loading.tsx for things to work properly. */}
       <Suspense fallback={<p>Loading product details...</p>}>
         <Product />
       </Suspense>
